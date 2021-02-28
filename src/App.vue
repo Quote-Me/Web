85b4cc56f4a4fe9ff7f566aaeeca77e653c8dcd8
Dragma <musaevilkin29@gmail.com>
2021-02-28 11:09:12 +0400

    Show random photo and quote

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=DotGothic16&display=swap');
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -16,17 +14,10 @@
   text-align: center;
   color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+body{
+    background: black;
+    padding: 0;
+    margin: 0;
 }
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <div class="home">
+        <div
+            class="image"
+            v-if="activePhoto"
+            :style="{ 'background-image': 'url(' + activePhoto + ')' }"
+        ></div>
+        <h2>
+            {{ activeQuote.quote }}
+        </h2>
+        <h4>
+            {{activeQuote.author}}
+        </h4>
+    </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+@Component({})
+export default class Home extends Vue {
+    activePhoto: string | null = null;
+    activeQuote: Object | null = null;
+
+    private randomElement(min: number, max: number): number {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    private async setActivePhoto() {
+        const photos = await fetch(
+            "https://raw.githubusercontent.com/Quote-Me/Data/main/photos.json"
+        ).then((r) => r.json());
+        const photo = photos[this.randomElement(0, photos.length - 1)];
+        this.activePhoto = photo.photo;
+    }
+
+    private async setActiveQuote(){
+        const quotes = await fetch("https://raw.githubusercontent.com/Quote-Me/Data/main/quotes.json").then((r) => r.json());
+        const quote = quotes[this.randomElement(0, quotes.length - 1)];
+        this.activeQuote = quote;
+    }
+    async mounted() {
+        await this.setActivePhoto();
+        await this.setActiveQuote();
+    }
+}
 </script>
+<style scoped>
+.image {
+    background-size: contain;
+    width: 100%;
+    height: 450px;
+    background-repeat: no-repeat;
+    background-position: center center;
+}
+h2, h4{
+    font-family: 'DotGothic16', sans-serif;
+}
+</style>
